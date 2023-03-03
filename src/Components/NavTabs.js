85bc56a4536ccf8592574c8ce2cf592b3c33a352
lissasimp <116848303+lissasimp@users.noreply.github.com>
2023-03-03 20:04:07 +0000

    added code to NavTabs

--- src/Components/NavTabs.js
+++ src/Components/NavTabs.js
@@ -1,3 +1,69 @@
 // Must be rendered across every page component of the site
 
 // Must contain a Navbar
+
+//I think Social media should be an extension of the home page (was originally Learn)
+
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function NavTabs() {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <NavLink
+          to="/"
+          end
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Home
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="about"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          About
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="blog"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Projects
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="contact"
+          end
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Contact
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="contact/learn"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Social Media 
+        </NavLink>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
