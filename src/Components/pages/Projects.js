eaf6c49550ca31cff9dfcb36724162a79a3df15b
lissasimp <116848303+lissasimp@users.noreply.github.com>
2023-03-05 14:13:41 +0000

    Added more details to projects

--- src/Components/pages/Projects.js
+++ src/Components/pages/Projects.js
@@ -8,12 +8,10 @@ function ProjectCard(props) {
   return (
 <div>
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={props.image} img alt={props.name}/>
+      <Card.Img variant="top" src={props.image} alt={props.name}/>
       <Card.Body>
         <Card.Title></Card.Title> {props.name}
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
         </Card.Text> {props.text}
         <Card.Link href="#">Deployed Site</Card.Link> {props.deployedLink}
         <Card.Link href="#">Github Repo</Card.Link> {props.githubRepo}
@@ -25,18 +23,3 @@ function ProjectCard(props) {
 
 export default ProjectCard;
 
-// return (
-//   <Wrapper>
-//     <Title>Friends List</Title>
-//     {friends.map(function(buddy) {
-//       return <FriendCard
-//         key={buddy.id}
-//         name={buddy.name + ", my pal"}
-//         image={buddy.image}
-//         occupation={buddy.occupation}
-//         location={buddy.location}
-//         doRemove={function() { removeFriend(buddy.id)} }
-//       />
-//     })}
-//   </Wrapper>
-// );
