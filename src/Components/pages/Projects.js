1c16c505eda3f7b69d8e8d31b7a50e3414e0ac6a
lissasimp <116848303+lissasimp@users.noreply.github.com>
2023-03-06 19:45:47 +0000

    links working projectcard

--- src/Components/pages/Projects.js
+++ src/Components/pages/Projects.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
+// import Row from "react-bootstrap/Card";
+// import Col from "react-bootstrap/Card";
 import projects from "../../projects.json";
 
 function ProjectCard(props) {
   return (
+   
     <div>
+   
       {projects.map((props,key) => (
         <Card style={{ width: '18rem' }}>
         <Card.Body key={key}>
@@ -13,12 +17,15 @@ function ProjectCard(props) {
           <Card.Text>
            {props.text}
           </Card.Text>
-          <Card.Link href="#">{props.deployedLink}</Card.Link> 
-          <Card.Link href="#">{props.githubRepo}</Card.Link> 
+          <Card.Link href={props.deployedLink}>Deployed Site</Card.Link> 
+          <Card.Link href={props.githubRepo}>Github Repo</Card.Link> 
         </Card.Body>
         </Card>
       ))}
+    
     </div>
+ 
+    
   );
 }
 
