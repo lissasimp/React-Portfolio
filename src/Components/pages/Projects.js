4843609999e879a3fa8e66d9ac6e71d1be9bc6de
lissasimp <116848303+lissasimp@users.noreply.github.com>
2023-03-06 20:35:22 +0000

    added key to top level

--- src/Components/pages/Projects.js
+++ src/Components/pages/Projects.js
@@ -6,27 +6,25 @@ import projects from "../../projects.json";
 
 function ProjectCard(props) {
   return (
-   
     <div>
-   
-      {projects.map((props,key) => (
+      {projects.map((props) => (
+        <Card.Body key={props.id}>
         <Card style={{ width: '18rem' }}>
-        <Card.Body key={key}>
+        
           <Card.Title>{props.name}</Card.Title> 
-          <Card.Img variant="top" src={props.image} img alt={props.name} />
+          {/* <Card.Img variant="top" src={props.image} img alt={props.name} /> */}
           <Card.Text>
            {props.text}
           </Card.Text>
           <Card.Link href={props.deployedLink}>Deployed Site</Card.Link> 
           <Card.Link href={props.githubRepo}>Github Repo</Card.Link> 
+          </Card>
         </Card.Body>
-        </Card>
       ))}
-    
     </div>
- 
-    
   );
+  
 }
 
+      
 export default ProjectCard;
