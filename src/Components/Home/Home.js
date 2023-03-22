aa1e77a41ee37afeb9616f96e9bc8def39141304
lissasimp <116848303+lissasimp@users.noreply.github.com>
2023-03-22 13:39:57 +0000

    social media buttons added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,44 +1,60 @@
-// Possibly put this on home page?
-// A good portfolio includes updated skills information and links to your web presence. You should update the following social media accounts as well:
-
-// LinkedIn
-// Make sure to update your LinkedIn Profile with the new skills you've acquired since the last time it was updated.
-
-// If you do not have a LinkedIn profile, please create one.
-
-// GitHub
-// Update your GitHub profile with pinned repositories featuring those same projects.
-
 import React from "react";
 import "./Home.css";
 import profile from "../../Images/profile.PNG";
+import Button from "react-bootstrap/Button";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 function Home() {
   return (
     <>
-    <section className="homeContainer">
+      <section className="homeContainer">
         <div className="row">
-            <div className="col-sm-12 col-lg-5">
-      <h2 style={{ fontSize: "3 rem" }}>Hi I'm </h2>
-      <h1 className="homeTitle"style={{ fontSize: "5rem" }}>
-        <span style={{ color: "orange" }}>Lissa</span>
-      </h1>
-      <h2 style={{ fontSize: " 3rem" }}>Front End Developer </h2>
-      {/* <p>I'm passionate about web design</p> */}
-      </div>
+          <div className="col-sm-12 col-lg-5">
+            <h2 style={{ fontSize: "3 rem" }}>Hi I'm </h2>
+            <h1 className="homeTitle" style={{ fontSize: "5rem" }}>
+              <span style={{ color: "orange" }}>Lissa</span>
+            </h1>
+            <h2 style={{ fontSize: " 3rem" }}>Front End Developer </h2>
+            {/* <p>I'm passionate about web design</p> */}
+            <div className="btnContainer">
+              <Button
+                className="smBtn"
+                style={{ fontSize: "1.5rem" }}
+                href="https://twitter.com/LissaSimpson17"
+              >
+                <FaTwitter />
+              </Button>{" "}
+              <Button
+                className="smBtn"
+                style={{ fontSize: "1.5rem" }}
+                href="https://www.linkedin.com/in/lissa-simpson-5004a3111/"
+              >
+                <FaLinkedin />
+              </Button>{" "}
+              <Button
+                className="smBtn"
+                style={{ fontSize: "1.5rem" }}
+                href="https://github.com/lissasimp"
+              >
+                <FaGithub />
+              </Button>{" "}
+            </div>
+          </div>
 
-      <div className="col-sm-12 col-lg-6">
-      <img
-        src={profile}
-        className="square"
-        alt=""
-        width="340px"
-        height="340px"
-        horizontalAlign= "center"
-      ></img>
-      </div>
-    </div>
-    </section>
+          <div className="col-sm-12 col-lg-6">
+            <img
+              src={profile}
+              className="square"
+              alt=""
+              width="340px"
+              height="340px"
+              horizontalAlign="center"
+            ></img>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
