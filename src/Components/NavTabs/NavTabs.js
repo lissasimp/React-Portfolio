b02fd8eaa5bc79dae539ac6a78c30c844da0f1ce
lissasimp <116848303+lissasimp@users.noreply.github.com>
2023-06-09 14:42:09 +0100

    changed order of nav links

--- src/Components/NavTabs/NavTabs.js
+++ src/Components/NavTabs/NavTabs.js
@@ -2,65 +2,60 @@ import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
-
-
 function NavTabs(props) {
   return (
     <Navbar id="navbar" variant="dark" collapseOnSelect expand="lg">
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav" >
+      <Navbar.Collapse id="basic-navbar-nav">
         <Container fluid>
           {/* <Nav className="me-auto"> */}
           <Navbar.Brand as={NavLink} to="/" id="myname">
             Lissa Simpson
-          
           </Navbar.Brand>
 
           {/*https://stackoverflow.com/questions/54843302/reactjs-bootstrap-navbar-and-routing-not-working-together*/}
           <section>
             <Nav className="justify-content-end">
-            <Nav.Link
-              id="links"
-              as={NavLink}
-              to="contact"
-              onClick={() => "Projects"}
-              className={
-                props.currentPage === "Projects"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-            >
-              Contact
-            </Nav.Link>
+              <Nav.Link
+                id="links"
+                as={NavLink}
+                to="about"
+                onClick={() => "about"}
+                className={
+                  props.currentPage === "About" ? "nav-link active" : "nav-link"
+                }
+              >
+                About
+              </Nav.Link>
 
-            <Nav.Link
-              id="links"
-              as={NavLink}
-              to="projects"
-              onClick={() => "Projects"}
-              className={
-                props.currentPage === "Projects"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-            >
-              Projects
-            </Nav.Link>
-
-            <Nav.Link
-              id="links"
-              as={NavLink}
-              to="about"
-              onClick={() => "about"}
-              className={
-                props.currentPage === "About" ? "nav-link active" : "nav-link"
-              }
-            >
-              About
-            </Nav.Link>
+              <Nav.Link
+                id="links"
+                as={NavLink}
+                to="projects"
+                onClick={() => "Projects"}
+                className={
+                  props.currentPage === "Projects"
+                    ? "nav-link active"
+                    : "nav-link"
+                }
+              >
+                Projects
+              </Nav.Link>
+              <Nav.Link
+                id="links"
+                as={NavLink}
+                to="contact"
+                onClick={() => "Projects"}
+                className={
+                  props.currentPage === "Projects"
+                    ? "nav-link active"
+                    : "nav-link"
+                }
+              >
+                Contact
+              </Nav.Link>
             </Nav>
           </section>
-          
         </Container>
       </Navbar.Collapse>
     </Navbar>
