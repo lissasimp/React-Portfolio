f513c49f6d96502da8d68ee9e3ec2b32a887d11e
lissasimp <116848303+lissasimp@users.noreply.github.com>
2023-06-23 19:00:50 +0100

    buttons centred

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
 import { Link } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import { FaTwitter } from "react-icons/fa";
@@ -14,77 +15,85 @@ import { FaMap } from "react-icons/fa";
 
 function Contact() {
   return (
-    <Container id="contactBox" >
+    <Container id="contactBox">
       <body>
-        <h2
-          className="title-font font-semibold text-yellow text-center"
-          style={{ paddingTop: "50px", fontSize: "40px" }}
-        >
-          We'd love to hear from you!
-        </h2>
-        <p
-          className="text-center"
-          style={{ padding: "40px", fontSize: "30px" }}
-        >
-          If you would like us to work with you on your next project, please get
-          in touch and tell us about what you’d like to achieve.
-        </p>
-        <div className="text-center">
-          <p style={{ fontSize: "25px" }}>
-            <FaEnvelope style={{ color: "#1DA1F2", marginRight: "10px" }} />
-            <a
-              onClick="javascript:window.open('mailto:lissasimpsonid@gmail.com', 'mail');event.preventDefault()"
-              href="mailto:lissasimpsonid@gmail.com"
-            >
-              lissasimpsonid@gmail.com
-            </a>
-          </p>
-          <p style={{ fontSize: "25px" }}>
-            <FaPhone style={{ color: "#1DA1F2", marginRight: "10px" }} />
-            <Link to="tel:07717824584">Call Me</Link>
-          </p>
-          <p style={{ fontSize: "25px" }}>
-            <FaMap
-              style={{
-                color: "#1DA1F2",
-                marginRight: "10px",
-                fontSize: "40px",
-              }}
-            />{" "}
-            Portsmouth, UK
-          </p>
-
+        <div>
+          <h2
+            className="title-font font-semibold text-yellow text-center"
+            style={{ paddingTop: "50px", fontSize: "40px" }}
+          >
+            We'd love to hear from you!
+          </h2>
           <p
             className="text-center"
-            style={{ fontSize: "25px", paddingTop: "30px" }}
+            style={{ padding: "40px", fontSize: "30px" }}
           >
-            Or connect with us:
+            If you would like us to work with you on your next project, please
+            get in touch and tell us about what you’d like to achieve.
           </p>
+          <div className="text-center">
+            <p style={{ fontSize: "25px" }}>
+              <FaEnvelope style={{ color: "#1DA1F2", marginRight: "10px" }} />
+              <a
+                onClick="javascript:window.open('mailto:lissasimpsonid@gmail.com', 'mail');event.preventDefault()"
+                href="mailto:lissasimpsonid@gmail.com"
+              >
+                lissasimpsonid@gmail.com
+              </a>
+            </p>
+            <p style={{ fontSize: "25px" }}>
+              <FaPhone style={{ color: "#1DA1F2", marginRight: "10px" }} />
+              <Link to="tel:07717824584">Call Me</Link>
+            </p>
+            <p style={{ fontSize: "25px" }}>
+              <FaMap
+                style={{
+                  color: "#1DA1F2",
+                  marginRight: "10px",
+                  fontSize: "40px",
+                }}
+              />{" "}
+              Portsmouth, UK
+            </p>
 
-          <div className="btnContainer" style={{ padding: "10px" }}>
-            <div className="col-md-12 text-center">
-              <Button
-                className="contactBtn"
-                style={{ marginRight: "2rem" }}
-                href="https://twitter.com/LissaSimpson17"
-              >
-                <FaTwitter style={{ fontSize: "3rem" }} />
-              </Button>{" "}
-              <Button
-                className="contactBtn"
-                style={{ marginRight: "2rem" }}
-                href="https://www.linkedin.com/in/lissasimpson/"
-              >
-                <FaLinkedin style={{ fontSize: "3rem" }} />
-              </Button>{" "}
-              <Button
-                className="contactBtn"
-                style={{ marginRight: "2rem" }}
-                href="https://github.com/lissasimp"
-              >
-                <FaGithub style={{ fontSize: "3rem" }} />
-              </Button>{" "}
-            </div>
+            <p
+              className="text-center"
+              style={{ fontSize: "25px", paddingTop: "30px" }}
+            >
+              Or connect with us:
+            </p>
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              height: "100%",
+              marginBottom: "100px"
+            }}
+          >
+            <Button
+              className="contactBtn"
+              style={{ marginRight: "1rem" }}
+              href="https://twitter.com/LissaSimpson17"
+            >
+              <FaTwitter style={{ fontSize: "3rem" }} />
+            </Button>{" "}
+            <Button
+              className="contactBtn"
+              style={{ marginRight: "1rem" }}
+              href="https://www.linkedin.com/in/lissasimpson/"
+            >
+              <FaLinkedin style={{ fontSize: "3rem" }} />
+            </Button>{" "}
+            <Button
+              className="contactBtn"
+              style={{ marginRight: "1rem" }}
+              href="https://github.com/lissasimp"
+            >
+              <FaGithub style={{ fontSize: "3rem" }} />
+            </Button>{" "}
           </div>
         </div>
       </body>
